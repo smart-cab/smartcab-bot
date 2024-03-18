88f5384f0f3bd40598b606c1014df209b9210bc0
Alchemmist <anton.ingrish@gmail.com>
2024-03-18 18:01:08 +0300

    done load statistics

--- smartcab/__init__.py
+++
@@ -1,7 +0,0 @@
-import aiohttp
-
-
-async def fetch_data(url):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            return await response.text()
--- smartcab/handlers/upload_statistics.py
+++ smartcab/handlers/upload_statistics.py
@@ -1,5 +1,5 @@
 import logging
-from smartcab import fetch_data
+import aiohttp
 from telegram import Update
 from telegram.ext import ContextTypes
 
@@ -13,11 +13,16 @@ async def upload_statistics(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await query.edit_message_text(
         text="Выполняется дамп базы данных. Пожалуйста подождите..."
     )
-    # url = 'http://localhost:5000/status'
-    # data = await fetch_data(url)
-    # print(data)
+
+    url = "http://localhost:5000/export_statistics"
+    async with aiohttp.ClientSession(trust_env=True) as session:
+        async with session.get(url) as response:
+            excel_file_content = await response.read()
+            with open("dumb.xlsx", "wb") as f:
+                f.write(excel_file_content)
+
     await query.edit_message_text(text="А вот и статитистика!")
     if effective_chat := update.effective_chat:
         await context.bot.send_document(
-            chat_id=effective_chat.id, document=open("path_to_file", "rb")
+            chat_id=effective_chat.id, document=open("dumb.xlsx", "rb")
         )
